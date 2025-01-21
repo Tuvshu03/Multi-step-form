1e719b371e920fd63f663dbd8062a60932b67424
Tuvshu03 <159090318+Tuvshu03@users.noreply.github.com>
2025-01-21 12:35:13 +0800

    first commit

---
+++ src/app/components/FinishedForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinishedForm = () => {
+  return (
+    <div>FinishedForm</div>
+  )
+}
+
+export default FinishedForm
---
+++ src/app/components/StepOne.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const StepOne = (props) => {
+    const [handleBackStep, handleNextStep] = props
+  return (
+    <div>StepOne</div>
+  )
+}
+
+export default StepOne
