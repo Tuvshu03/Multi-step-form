d05d7c811bd33a1c2c4db901a7412688dc6032f3
Tuvshu03 <159090318+Tuvshu03@users.noreply.github.com>
2025-04-04 10:37:21 +0800

    finished

--- src/app/components/MultiStepForm.js
+++ src/app/components/MultiStepForm.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import StepOne from "./StepOne";
 import StepTwo from "./StepTwo";
 import StepThree from "./StepThree";
